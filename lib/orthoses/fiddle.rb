47b676fa128ceb39156741d6dc958dfaa3dfc3e0
ksss <co000ri@gmail.com>
2023-11-05 22:56:16 +0900

    bundle gem orthoses-fiddle

---
+++ lib/orthoses/fiddle.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "fiddle/version"
+
+module Orthoses
+  module Fiddle
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/orthoses/fiddle/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Orthoses
+  module Fiddle
+    VERSION = "0.1.0"
+  end
+end
